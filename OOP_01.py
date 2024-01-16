468fa3a26cd6497400ec3675d861423ff8a6b626
FahithKRM <FahithKRM>
2024-01-17 00:01:53 +0530

    Modified OOP_01.py

--- OOP_01.py
+++ OOP_01.py
@@ -1,23 +1,43 @@
+# ###Example :
+# class Trip :
+
+#     def __init__(self):
+#         self.amount = None
+#         self.vehicle = None
+#         self.hours = None
+
+#     def stoped(self) :
+#         print("I am finishing my trip...")
+#         print("----My Trip Details : -----")
+#         print(f"Amount : $ {self.amount}")
+#         print(f"Vehicle : {self.vehicle}")
+#         print(f"Hours : {self.hours} hours")
+
+
+
+# my_trip = Trip()
+
+# my_trip.amount = 2000
+# my_trip.vehicle = "Bus"
+# my_trip.hours = 7
+
+# my_trip.stoped()
+
+#### Update above example with reduce line of codes
 class Trip :
 
-    def __init__(self):
-        self.amount = None
-        self.vehicle = None
-        self.hours = None
+    # as constractor
+    def __init__(self, amount, vehicle, hours):
+        self.amount = amount
+        self.vehicle = vehicle
+        self.hours = hours
 
-    def stop(self) :
+    def stoped(self) :
         print("I am finishing my trip...")
         print("----My Trip Details : -----")
         print(f"Amount : $ {self.amount}")
         print(f"Vehicle : {self.vehicle}")
         print(f"Hours : {self.hours} hours")
 
-
-
-my_trip = Trip()
-
-my_trip.amount = 2000
-my_trip.vehicle = "Bus"
-my_trip.hours = 7
-
-my_trip.stop()
+my_trip = Trip(1500, "Car", 4)
+my_trip.stoped()
